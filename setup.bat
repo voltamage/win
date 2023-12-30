403dd9bc5ec427ec36f248357269f3069e3e60fd
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-29 21:31:18 -0800

    git clone step added

--- setup.bat
+++ setup.bat
@@ -1,29 +1,12 @@
 powershell -command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 powershell -command "iwr -useb get.scoop.sh | iex"
 
+powershell -command "scoop install git"
+powershell -command "git clone git@github.com:voltamage/win.git"
+
 powershell -command "scoop install gsudo"
 powershell -command "sudo Rename-Computer -NewName 'WINDOWS'"
 
 powershell -command "sudo Install-Module PSWindowsUpdate"
 powershell -command "sudo Install-WindowsUpdate"
-pause
 
-REM echo Install VPN
-REM powershell -command "scoop install main/git"
-REM powershell -command "scoop install main/gsudo"
-REM powershell -command "scoop bucket add nonportable"
-REM powershell -command "sudo scoop install nonportable/mullvadvpn-np"
-REM
-REM echo Set up vpn and come back to this window when you are ready
-REM pause
-REM
-REM echo Install Browser
-REM powershell -command "scoop bucket add extras"
-REM powershell -command "scoop install librewolf"
-REM
-REM echo Install Neovim
-REM powershell -command "scoop bucket add extras"
-REM powershell -command "scoop bucket add versions"
-REM powershell -command "scoop install extras/vcredist"
-REM powershell -command "scoop install neovim-nightly"
-REM pause
