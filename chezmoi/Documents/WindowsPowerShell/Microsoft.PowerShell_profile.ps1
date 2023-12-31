4597a9c423b41c32f588b65f5595c91f73cd1c6f
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-30 19:52:11 -0800

    next update

--- chezmoi/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ chezmoi/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,9 @@
-Invoke-Expression (&starship init powershell)
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (&scoop-search --hook)
+Invoke-Expression (&starship init powershell)
--- komo.bat
+++
@@ -1,3 +0,0 @@
-powershell -command "sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1"
-powershell -command "scoop install komorebi whkd"
-pause
