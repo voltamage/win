fc59a52e22ffcef086886c91c07ee9ebe4fb3178
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-31 14:34:22 -0800

    updated setup

--- setup.bat
+++ setup.bat
@@ -1,20 +1,21 @@
 powershell -command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 powershell -command "iwr -useb get.scoop.sh | iex"
-echo Close this window and restart this script
+powershell -command "$env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')"
+powershell -command "Get-ChildItem env:"
 pause
 
-powershell -command "scoop install chezmoi git gsudo"
-echo Close this window and restart this script
+powershell -command "scoop install git"
+powershell -command "$env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')"
+powershell -command "Get-ChildItem env:"
 pause
-
 powershell -command "git clone git@github.com:voltamage/win.git"
-powershell -command "chezmoi apply"
 
+powershell -command "scoop install gsudo"
 powershell -command "sudo Rename-Computer -NewName 'WINDOWS'"
-powershell -command "sudo Install-Module PSWindowsUpdate"
-echo Don't restart when asked during the windows update, wait until WSL features come online
-pause
-powershell -command "sudo Install-WindowsUpdate"
+REM powershell -command "sudo Install-Module PSWindowsUpdate"
+REM echo Don't restart when asked during the windows update, wait until WSL features come online
+REM pause
+REM powershell -command "sudo Install-WindowsUpdate"
 
 powershell -command "sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart"
 powershell -command "sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart"
@@ -26,4 +27,13 @@ powershell -command "wsl --update"
 powershell -command "scoop bucket add extras"
 powershell -command "scoop install extras/archwsl"
 echo Start arch and hard kill fail loop, update keyring before full system, come back to this window after
+pause
+
+powershell -command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0"
+powershell -command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
+
+powershell -command "scoop bucket add nerd-fonts"
+powershell -command "scoop bucket add versions"
+powershell -command "scoop install chezmoi nerd-fonts/JetBrainsMono-NF-Mono extras/psreadline scoop-search starship versions/windows-terminal-preview zoxide"
+powershell -command "chezmoi apply"
 pause
